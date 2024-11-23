d1753c5552b2dd4ae40acacafba77ec909e5ff49
squireChuck <squirechucks.codequest@gmail.com>
2024-11-22 21:09:06 -0500

    render all asset market values

--- src/components/MyChart/index.tsx
+++ src/components/MyChart/index.tsx
@@ -1,31 +1,42 @@
 import cashFlowData from '../../assets/cash-flow-testing.csv';
-import { Bar } from '../common/Chart';
+import { Line } from '../common/Chart';
 // import styles from './index.module.scss';
 
 
 // https://www.chartjs.org/docs/latest/samples/line/line.html
-
 function Chart() {
   console.log('', cashFlowData);
+  const labels = Object.keys(cashFlowData[0]).filter(key => !['Asset', 'Attribute'].includes(key)).sort((a, b) => {
+    return Number.parseInt(a) - Number.parseInt(b);
+  });
+  const datasets = cashFlowData.reduce((accum, asset) => {
+    return asset.Attribute === 'Market Value'
+      ? 
+        [
+          ...accum,
+          {
+            label: `Asset ${asset.Asset}`,
+            data: labels.map(label => asset[label]),
+            borderWidth: 1
+          }
+        ]
+      : accum;
+  }, []);
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
+    labels,
+    datasets
   };
-const options = {
-  scales: {
-    y: {
-      beginAtZero: true
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
     }
-  }
-};
+  };
 
   return (
     <>
-      <Bar data={data} options={options} />
+      <Line data={data} options={options} />
     </>
   );
 }
